baaae62d570fb82797f5a41a66e37ca348ecc1b7
Geon_27 <yukgeon98527@gmail.com>
2022-10-29 11:22:31 +0900

    1029

--- Scene-Trans2/SecondViewController.swift
+++ Scene-Trans2/SecondViewController.swift
@@ -12,4 +12,9 @@ class SecondViewController: UIViewController {
     @IBAction func back(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true)
     }
+    
+    
+    @IBAction func Back2(_ sender: Any) {
+        _=self.navigationController?.popViewController(animated: true)
+    }
 }
